c95aaddbc93aeb5ba71181b1fbbcc1077505e7c4
Yuka Kato <satoimo.to.imo@gmail.com>
2016-11-27 13:08:41 +0900

    Remove unused comment

--- features/support/env.rb
+++ features/support/env.rb
@@ -24,20 +24,6 @@ ENV["RAILS_ENV"] ||= "test"
 #
 ActionController::Base.allow_rescue = false
 
-# You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
-# See the DatabaseCleaner documentation for details. Example:
-#
-#   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-#     # { :except => [:widgets] } may not do what you expect here
-#     # as Cucumber::Rails::Database.javascript_strategy overrides
-#     # this setting.
-#     DatabaseCleaner.strategy = :truncation
-#   end
-#
-#   Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity', '~@javascript') do
-#     DatabaseCleaner.strategy = :transaction
-#   end
-#
 Before do
   ActiveRecord::FixtureSet.reset_cache
   fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
